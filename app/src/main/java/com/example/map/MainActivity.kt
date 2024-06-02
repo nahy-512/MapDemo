c1bc53b84d040726874118c1023e050254ea768a
Nahyun Kim <hedghog1004@naver.com>
2024-06-03 04:10:24 +0900

    ✨ Feat: 네이버 지도 추가

--- app/src/main/java/com/example/map/MainActivity.kt
+++ app/src/main/java/com/example/map/MainActivity.kt
@@ -10,10 +10,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.map.databinding.ActivityMainBinding
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource: FusedLocationSource
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +29,56 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(binding.root)
 
-        binding.buttonStartLocationUpdates.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(
-                    applicationContext,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) != PackageManager.PERMISSION_GRANTED
+        if (!hasPermission()) {
+            requestLocationPermission()
+        } else {
+            initMapView()
+        }
+        initClickListeners()
+    }
+
+    // 위치 권한이 있을 경우 true, 없을 경우 false 반환
+    private fun hasPermission(): Boolean {
+        for (permission in PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                != PackageManager.PERMISSION_GRANTED
             ) {
-                ActivityCompat.requestPermissions(
-                    this@MainActivity,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    REQUEST_CODE_LOCATION_PERMISSION
-                )
+                return false
+            }
+        }
+        return true
+    }
+
+    // 위치 권한 요청
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this@MainActivity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_CODE_LOCATION_PERMISSION
+        )
+    }
+
+    private fun initClickListeners() {
+        binding.buttonStartLocationUpdates.setOnClickListener {
+            if (!hasPermission()) {
+                requestLocationPermission()
             } else {
                 startLocationService()
             }
         }
         binding.buttonStopLocationUpdates.setOnClickListener { stopLocationService() }
+    }
+
+    private fun initMapView() {
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
+
+        // fragment의 getMapAsync() 메서드로 OnMapReadyCallback 콜백을 등록하면 비동기로 NaverMap 객체를 얻을 수 있다.
+        mapFragment.getMapAsync(this)
+        locationSource = FusedLocationSource(this, REQUEST_CODE_LOCATION_PERMISSION)
     }
 
     override fun onRequestPermissionsResult(
@@ -46,7 +87,7 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.size > 0) {
+        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.isNotEmpty()) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 startLocationService()
             } else {
@@ -86,7 +127,21 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        // 현재 위치
+        naverMap.locationSource = locationSource
+        // 현재 위치 버튼 기능
+        naverMap.uiSettings.isLocationButtonEnabled = true
+        // 위치를 추적하면서 카메라도 따라 움직인다.
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+    }
+
     companion object {
         private const val REQUEST_CODE_LOCATION_PERMISSION = 1
+        private val PERMISSIONS = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
     }
 }
