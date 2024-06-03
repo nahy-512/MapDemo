76115f9f6792724e334b8b3af8002bcf5b620c14
Nahyun Kim <hedghog1004@naver.com>
2024-06-03 14:33:47 +0900

    ✨ Feat: 좌표로 polyLine 그리기 테스트

--- app/src/main/java/com/example/map/MainActivity.kt
+++ app/src/main/java/com/example/map/MainActivity.kt
@@ -4,23 +4,35 @@ import android.Manifest
 import android.app.ActivityManager
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.map.databinding.ActivityMainBinding
+import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapFragment
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.PolylineOverlay
 import com.naver.maps.map.util.FusedLocationSource
-
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var binding: ActivityMainBinding
     private lateinit var naverMap: NaverMap
     private lateinit var locationSource: FusedLocationSource
+
+    private val polyline = PolylineOverlay()
+    private val coords = mutableListOf(
+        LatLng(37.55315, 126.972533), // 서울역
+        LatLng(37.561159, 127.035505), // 왕십리역
+        LatLng(37.540408, 127.069231), // 건대 입구역
+        LatLng(37.54718, 127.047413), // 뚝섬역
+        LatLng(37.496068, 127.028506), // 강남역
+        LatLng(37.394726159, 127.111209047), // 판교역
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +47,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             initMapView()
         }
         initClickListeners()
+        initPolyLine()
     }
 
     // 위치 권한이 있을 경우 true, 없을 경우 false 반환
@@ -79,6 +92,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         // fragment의 getMapAsync() 메서드로 OnMapReadyCallback 콜백을 등록하면 비동기로 NaverMap 객체를 얻을 수 있다.
         mapFragment.getMapAsync(this)
         locationSource = FusedLocationSource(this, REQUEST_CODE_LOCATION_PERMISSION)
+    }
+
+    private fun initPolyLine() {
+        polyline.coords = coords
+        polyline.color = Color.MAGENTA
+        polyline.width = 8
     }
 
     override fun onRequestPermissionsResult(
@@ -135,6 +154,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         naverMap.uiSettings.isLocationButtonEnabled = true
         // 위치를 추적하면서 카메라도 따라 움직인다.
         naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        // 선 연결
+//        updatePolygon()
+        polyline.map = naverMap
     }
 
     companion object {
